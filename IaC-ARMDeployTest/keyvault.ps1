c671a2aea95370b6f001ced43a7e059060b34347
Blair <boriley@ricoh.co.nz>
2021-09-15 13:46:51 +1200

    changed length

--- IaC-ARMDeployTest/keyvault.ps1
+++ IaC-ARMDeployTest/keyvault.ps1
@@ -14,10 +14,10 @@ $secrets = Import-Csv 'IaC-ARMDeployTest\keyvault.csv'
 Add-Type -AssemblyName 'System.Web'
 function New-RandomPassword() {
     param (
-        [int]$size
+        [int]$NonAlpha
     )
-    $minLength = 15 ## characters
-    $maxLength = $size ## characters
+    $minLength = 14 ## characters
+    $NonAlpha = $size ## characters
     $length = Get-Random -Minimum $minLength -Maximum $maxLength
     $nonAlphaChars = $minLength
     $password = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
@@ -26,7 +26,7 @@ function New-RandomPassword() {
 }
 foreach ($secret in $secrets) {
     If ($secret.secret -eq 'random') {
-        $tmpSecret = ConvertTo-SecureString (New-RandomPassword -size 14) -AsPlainText -Force
+        $tmpSecret = ConvertTo-SecureString (New-RandomPassword -size 3) -AsPlainText -Force
     }
     Else {
         $tmpSecret = ConvertTo-SecureString $secret.secret -AsPlainText -Force
